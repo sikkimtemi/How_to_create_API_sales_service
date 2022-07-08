92d8176bece90b2fea920d81de5f0c13b6bb3248
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-07-08 17:41:51 +0900

    余分な記述を除去

--- 3-Mockups/fm_mail_frontend/src/vite-env.d.ts
+++ 3-Mockups/fm_mail_frontend/src/vite-env.d.ts
@@ -1,17 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_DELETE_USER_BASE_URL: string;
-  readonly VITE_DYNAMODB_BASE_URL: string;
-  readonly VITE_OAUTH_DOMAIN: string;
-  readonly VITE_OAUTH_REDIRECT_SIGN_IN: string;
-  readonly VITE_OAUTH_REDIRECT_SIGN_OUT: string;
-  readonly VITE_REGION: string;
-  readonly VITE_STRIPE_BASE_URL: string;
-  readonly VITE_USER_POOL_ID: string;
-  readonly VITE_USER_POOL_WEB_CLIENT_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
