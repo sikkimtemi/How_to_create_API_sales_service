37045f863d63a402cf1beeb3223a0810c0048aef
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-07-21 15:36:59 +0900

    ユーザー属性の利用

--- 6-CreateApiKey/fm_mail_frontend/src/routes/MyPage.tsx
+++ 6-CreateApiKey/fm_mail_frontend/src/routes/MyPage.tsx
@@ -1,12 +1,18 @@
 import { FC } from 'react';
+import { useAtom } from 'jotai';
 import Notice from '../components/Notice';
 import Spacer from '../components/Spacer';
 import AuthenticatedLayout from '../layouts/AuthenticatedLayout';
+import stateUserAttribute from '../atom/UserAttribute';
 
 // マイページ
 const MyPage: FC = () => {
+  // ユーザー属性から現在のプランを取り出す
+  const [userAttribute] = useAtom(stateUserAttribute);
+  const planType = userAttribute?.planType;
   const title = '現在のプラン';
-  const message = `ご利用中のプランはFREEプランです。`;
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  const message = `ご利用中のプランは${planType}プランです。`;
 
   return (
     <AuthenticatedLayout>
