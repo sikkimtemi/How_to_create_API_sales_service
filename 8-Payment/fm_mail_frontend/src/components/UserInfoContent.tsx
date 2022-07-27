ae4fd89ec3b5096cef12a450f08398b0350e89ba
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-07-27 18:03:24 +0900

    8章の差分を実装

--- 8-Payment/fm_mail_frontend/src/components/UserInfoContent.tsx
+++ 8-Payment/fm_mail_frontend/src/components/UserInfoContent.tsx
@@ -1,11 +1,15 @@
 import { FC, useState } from 'react';
 import { useAtom } from 'jotai';
 import Modal from 'react-modal';
+import stateUserAttribute from '../atom/UserAttribute';
 import Image from '../svg/undraw_browsing_re_eycn.svg';
 import Spacer from './Spacer';
-import stateUserAttribute from '../atom/UserAttribute';
+import openBillingPortal from '../function/StripeUtil';
+import stateCurrentUser from '../atom/User';
 
 const UserInfoContent: FC = () => {
+  const [user] = useAtom(stateCurrentUser);
+
   // ユーザー属性からemailと現在のプランを取り出す
   const [userAttribute] = useAtom(stateUserAttribute);
   const email = userAttribute?.email;
@@ -51,7 +55,19 @@ const UserInfoContent: FC = () => {
         <div className="mt-5 flex items-center justify-between sm:col-span-2">
           <button
             type="button"
-            className="inline-flex rounded border-0 bg-red-400 py-3 px-6 text-lg text-white hover:bg-red-500 focus:outline-none active:bg-red-600"
+            className={`inline-flex rounded border-0 bg-blue-500 py-3 px-6 text-lg text-white hover:bg-blue-600 focus:outline-none active:bg-blue-700 ${
+              planType === 'PRO' ? '' : 'hidden'
+            }`}
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onClick={() => openBillingPortal(user)}
+          >
+            カスタマーポータル
+          </button>
+          <button
+            type="button"
+            className={`inline-flex rounded border-0 bg-red-400 py-3 px-6 text-lg text-white hover:bg-red-500 focus:outline-none active:bg-red-600 ${
+              planType === 'FREE' ? '' : 'hidden'
+            }`}
             onClick={() => setIsOpen(true)}
           >
             アカウント削除
--- 8-Payment/fm_mail_frontend/src/vite-env.d.ts
+++ 8-Payment/fm_mail_frontend/src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_OAUTH_REDIRECT_SIGN_IN: string;
   readonly VITE_OAUTH_REDIRECT_SIGN_OUT: string;
   readonly VITE_REGION: string;
+  readonly VITE_STRIPE_BASE_URL: string;
   readonly VITE_USER_POOL_ID: string;
   readonly VITE_USER_POOL_WEB_CLIENT_ID: string;
 }
